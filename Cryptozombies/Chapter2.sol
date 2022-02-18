6c055cbe9daadf88a8f68cd5abbcb7ef025fc588
Rahul <prathampathak14@gmail.com>
2022-02-19 01:52:36 +0530

    Solidity Learnings

---
+++ Cryptozombies/Chapter2.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier:UNLICENSED
+pragma solidity ^0.8.4;
+
+//use the contract keyword
+//create an empty contract called Zombie Factory
+contract ZombieFactory{
+
+}
---
+++ Cryptozombies/Lesson 1/Chapter1.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier:UNLICENSED
+/*
+First thing we need to do is to mention license type
+if its Open sourced then use MIT
+  */
+  //first line of code contains pragma 
+  //This is basically a way to tell remix which version of solidity to use
+  // Why??
+  // because, the development of solidity is so fast that a new version is released almost every week and things keep breaking. ////To be sure, the solidity compiler version should be mentioned on the top of the file.
+  pragma solidity >=0.7.0 <0.9.0;
+contract HelloWorld {
+    
+}
+/*
+The next thing is the keyword contract 
+Programs on solidity are called contracts. A contract keyword is exactly similar to the class keyword you would have encountered on js/py/java.
+  
+  */
+
+
+
